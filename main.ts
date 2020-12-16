78ee7dfb1a0a8a8295a6530a30dd46cb997ab7c6
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2020-12-16 13:44:56 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,59 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # # . # #
+        . . . . .
+        # # # # #
+        # . . . #
+        . # # # .
+        `)
+    soundExpression.giggle.play()
+})
+
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    soundExpression.happy.play()
+})
+
+let timer = 0
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . # # # .
+    . . . . .
+    `)
+soundExpression.hello.play()
+
+basic.forever(function () {
+    basic.pause(1000)
+    timer = 1
+    if (timer == 20) {
+        basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            . # # # .
+            . . . . .
+            `)
+        soundExpression.sad.play()
+    }
+    if (timer == 40) {
+        soundExpression.mysterious.play()
+        music.setBuiltInSpeakerEnabled(false)
+        while (true) {
+            basic.showLeds(`
+                . # # # .
+                # . # . #
+                # # # # #
+                . # # # .
+                . # # # .
+                `)
+        }
+    }
+})
